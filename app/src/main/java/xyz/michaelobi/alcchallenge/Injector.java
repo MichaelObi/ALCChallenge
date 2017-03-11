bb93bcc25bb617ca620f7e1bc8626fa15166d786
Michael Obi <ask4myk@gmail.com>
2017-03-11 21:22:12 +0100

    Add access token to avoid rate limiting

--- app/src/main/java/xyz/michaelobi/alcchallenge/Injector.java
+++ app/src/main/java/xyz/michaelobi/alcchallenge/Injector.java
@@ -1,6 +1,12 @@
 package xyz.michaelobi.alcchallenge;
 
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -15,6 +21,7 @@ import xyz.michaelobi.alcchallenge.data.remote.GithubService;
 public class Injector {
     private static OkHttpClient okHttpClient;
     private static Retrofit retrofitInstance;
+    private static GithubService githubService;
 
     public static Retrofit provideRetrofit() {
         if (retrofitInstance == null) {
@@ -30,13 +37,24 @@ public class Injector {
 
     public static OkHttpClient provideOkHttpClient() {
         if (okHttpClient == null) {
-            okHttpClient = new OkHttpClient.Builder().build();
+            okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            HttpUrl httpUrl = chain.request().url();
+                            HttpUrl newUrl = httpUrl.newBuilder()
+                                    .addQueryParameter("access_token", "0580426c42f7f9b2b915cd6c989315813559375f")
+                                    .build();
+                            Request newRequest = chain.request().newBuilder()
+                                    .url(newUrl)
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    }).build();
         }
 
         return okHttpClient;
     }
-
-    private static GithubService githubService;
 
     public static GithubService provideGithubService() {
         if (githubService == null) {
